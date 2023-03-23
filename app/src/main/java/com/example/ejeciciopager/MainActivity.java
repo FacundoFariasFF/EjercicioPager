c9924ca8fc79fe5ed8f429116c0e886fd420a4c1
FacundoFariasFF <facundofarias.sistemas@gmail.com>
2023-03-23 13:58:20 -0300

    cambios 23/03/23

--- app/src/main/java/com/example/ejeciciopager/MainActivity.java
+++ app/src/main/java/com/example/ejeciciopager/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.ejeciciopager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
@@ -21,16 +23,45 @@ public class MainActivity extends FragmentActivity {
     private FragmentStateAdapter pagerAdapter;
 
 
-    String [] nombres = {"Marco","Marisol","Roberto"};
-    List<String> listaNombres = Arrays.asList(nombres);
+    //String [] nombres = {"Roberto","Facundo"};
+   // List<String> listaNombres = Arrays.asList(nombres);
+    List<String> listapilotos = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listapilotos.add("rossi");
+        listapilotos.add("marquez");
+        listapilotos.add("lorenzo");
         viewPager = findViewById(R.id.pager);
-        pagerAdapter = new ScreenSlidePagerAdapter(this,listaNombres);
+
+
+        //aca va el codigo para capturar la posicion del pager
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback(){
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels){
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);}
+            @Override
+            public void onPageSelected(int position){
+                super.onPageSelected(position);
+                Toast.makeText(MainActivity.this,"fragment nro: "+position,Toast.LENGTH_SHORT).show();
+
+                if (position==listapilotos.size()-1){
+                    //listaNombres.add(4,"Messi");
+                    listapilotos.add("messi");
+                    pagerAdapter.notifyDataSetChanged();
+                }
+
+            }
+           @Override
+            public void onPageScrollStateChanged(int state){
+                super.onPageScrollStateChanged(state);
+            }
+        });
+
+        pagerAdapter = new ScreenSlidePagerAdapter(this,listapilotos);
         viewPager.setAdapter(pagerAdapter);
 
         //FragmentA.newInstance(listaNombres);
